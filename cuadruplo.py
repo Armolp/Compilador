9f2ed14fa78ba0cbd12f34e25f1005ebb7a36665
Unknown <reginagallardo1@hotmail.com>
2017-11-12 14:47:21 -0600

    clase cuadruplo

---
+++ cuadruplo.py
@@ -0,0 +1,7 @@
+class cuadruplo:
+    def __init__(self, dir, accion, arg1, arg2, arg3):
+        self.dir = dir
+        self.accion = accion
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
