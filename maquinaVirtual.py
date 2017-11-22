631d5ab9fc0357195823d61e11dbdeca53680eaa
Unknown <reginagallardo1@hotmail.com>
2017-11-21 23:18:32 -0600

    maquina virtual terminada

--- maquinaVirtual.py
+++ maquinaVirtual.py
@@ -1,53 +1,10 @@
 from Compilador.cuadruplo import *
 from Compilador.dirFunc import *
 from Compilador.memoriaVirtual import *
+from tkinter import *
+from math import sin, cos
+import time
 
-var1 =Var(0,'int', 0)
-var2 =Var(3,'int', 1)
-var3 =Var(1,'int', 2)
-var4 =Var('x','int', 2000)
-var5 =Var('y','int', 2000)
-var6 =Var('func','int', 1000)
-
-func1 = Func('const','constante',0)
-func1.varTable.append(var1)
-func1.varTable.append(var2)
-func1.varTable.append(var3)
-
-func2 = Func('glob','global',1)
-func2.varTable.append(var6)
-
-func3 = Func('loc','local',2)
-func3.varTable.append(var4)
-func3.varTable.append(var5)
-
-dir = DirFunc()
-dir.functions.append(func1)
-dir.functions.append(func2)
-dir.functions.append(func3)
-
-cuads =[]
-
-cuads.append(cuadruplo(len(cuads),'goto','none', 'none', 13))
-cuads.append(cuadruplo(len(cuads),'==',2000, 0, 8000))
-cuads.append(cuadruplo(len(cuads),'gotoF',8000, 'none',5))
-cuads.append(cuadruplo(len(cuads),'return',0, 'none', 'none'))
-cuads.append(cuadruplo(len(cuads),'goto','none', 'none', 12))
-cuads.append(cuadruplo(len(cuads),'era',1000, 'none', 'none'))
-cuads.append(cuadruplo(len(cuads),'-',2000, 2, 6000))
-cuads.append(cuadruplo(len(cuads),'param',6000, 'none', 'none'))
-cuads.append(cuadruplo(len(cuads),'gosub',1, 'none', 'none'))
-cuads.append(cuadruplo(len(cuads),'=',1000, 'none', 6001))
-cuads.append(cuadruplo(len(cuads),'+',2000, 6001, 6002))
-cuads.append(cuadruplo(len(cuads),'return',6002, 'none', 'none'))
-cuads.append(cuadruplo(len(cuads),'EndProc','none', 'none', 'none'))
-cuads.append(cuadruplo(len(cuads),'era',1000, 'none', 'none'))
-cuads.append(cuadruplo(len(cuads),'param',1,'none', 'none'))
-cuads.append(cuadruplo(len(cuads),'gosub',1, 'none', 'none'))
-cuads.append(cuadruplo(len(cuads),'=',1000, 'none', 6000))
-cuads.append(cuadruplo(len(cuads),'=',6000, 'none', 2000))
-cuads.append(cuadruplo(len(cuads),'print',2000, 'none', 'none'))
-cuads.append(cuadruplo(len(cuads),'END','none', 'none', 'none'))
 
 class parametros():
     def __init__(self,value,type):
@@ -66,6 +23,10 @@ class maquinaVirtual():
         for i in range(0,len(self.fd.functions)):
             self.memVirtual.setFunctionValues(self.fd.functions[i])
         self.parametros = []
+        self.tk = Tk()
+        self.c = Canvas(self.tk, width=500, height=500)
+        self.c.pack()
+
     def goto(self,cuad):
         self.cuadActual = cuad.arg3-1
     def gotoF(self,cuad,memoria):
@@ -81,6 +42,7 @@ class maquinaVirtual():
         self.cuadReturn.append(self.cuadActual)
         self.run(begin,'EndProc')
         self.cuadActual = self.cuadReturn.pop()
+
     def param(self,cuad,memoria):
         valor = memoria.getValue(cuad.arg1)
         tipo = memoria.getType(cuad.arg1)
@@ -92,6 +54,9 @@ class maquinaVirtual():
         memoria.setValue(value,dir)
     def verify(self):
         print("does verify")
+    def era(self, cuad):
+        self.returnDir.append(cuad.arg1)
+
     def opArit(self,cuad,memoria):
         arg1 = memoria.getValue(cuad.arg1)
         arg2 = memoria.getValue(cuad.arg2)
@@ -148,8 +113,48 @@ class maquinaVirtual():
     def asign(self,cuad,memoria):
         value = memoria.getValue(cuad.arg1)
         memoria.setValue(value,cuad.arg3)
-    def era(self,cuad):
-        self.returnDir.append(cuad.arg1)
+
+    def printVar(self,cuad,memoria):
+        value = memoria.getValue(cuad.arg1)
+        print(value)
+    def cosin(self,cuad,memoria):
+        arg1 = memoria.getValue(cuad.arg1)
+        arg1 = memoria.fixType(cuad.arg1,arg1)
+        dir = cuad.arg3
+        accion = cuad.accion
+        if accion == 'cos':
+            ans = cos(arg1)
+        else:
+            ans = sin(arg1)
+        memoria.setValue(ans, dir)
+    def draw(self,cuad,memoria):
+        arg1 = memoria.getValue(cuad.arg1)
+        x = memoria.fixType(cuad.arg1, arg1)
+        arg2 = memoria.getValue(cuad.arg2)
+        y = memoria.fixType(cuad.arg2, arg2)
+        accion = cuad.accion
+        if accion == 'drawPoint':
+            r = 1
+        else:
+            arg3 = memoria.getValue(cuad.arg3)
+            r = memoria.fixType(cuad.arg3, arg3)
+        self.c.create_oval(x - r, y - r, x + r, y + r, fill='black')
+
+    def draw2(self,cuad1,cuad2,memoria):
+        arg1 = memoria.getValue(cuad1.arg1)
+        x1 = memoria.fixType(cuad1.arg1, arg1)
+        arg2 = memoria.getValue(cuad1.arg2)
+        y1 = memoria.fixType(cuad1.arg2, arg2)
+        arg3 = memoria.getValue(cuad2.arg1)
+        x2 = memoria.fixType(cuad2.arg1, arg3)
+        arg4 = memoria.getValue(cuad2.arg2)
+        y2 = memoria.fixType(cuad2.arg2, arg4)
+        accion = cuad1.accion
+        if accion == 'rect1':
+            self.c.create_rectangle(x1,y1,x2,y2, fill='black')
+        else:
+            self.c.create_line(x1,y1,x2,y2)
+
     def vaciarParametros(self,memoria):
         self.parametros.reverse()
         while self.parametros:
@@ -165,9 +170,7 @@ class maquinaVirtual():
             elif tipo == 'char':
                 dir = 5000 + len(memoria.memLocal.varChar)
             memoria.setValue(valor,dir)
-    def printVar(self,cuad,memoria):
-        value = memoria.getValue(cuad.arg1)
-        print(value)
+
     def run(self,begin,end):
         memoria = memoriaVirtual()
         memoria.memGlobal = self.memVirtual.memGlobal
@@ -177,7 +180,7 @@ class maquinaVirtual():
         while self.cuadruplos[self.cuadActual].accion != end:
             cuadruplo = self.cuadruplos[self.cuadActual]
             accion = cuadruplo.accion
-            #print('do: ' + str(self.cuadruplos[self.cuadActual]))
+            print('do: ' + str(self.cuadruplos[self.cuadActual]))
             if accion == 'goto':
                 self.goto(cuadruplo)
             elif accion == 'gotoF':
@@ -204,7 +207,19 @@ class maquinaVirtual():
                 self.asign(cuadruplo,memoria)
             elif accion == 'print':
                 self.printVar(cuadruplo,memoria)
-
+            elif accion == 'cos' or accion == 'sin':
+                self.cosin(cuadruplo,memoria)
+            elif accion == 'drawPoint' or accion == 'drawCircle':
+                self.draw(cuadruplo,memoria)
+            elif accion == 'rect1' or accion == 'from':
+                self.cuadActual+=1
+                cuad2 = self.cuadruplos[self.cuadActual]
+                self.draw2(cuadruplo,cuad2,memoria)
+            elif accion == 'update':
+                time.sleep(0.01)
+                self.tk.update()
+            elif accion =='delete':
+                self.c.delete(ALL)
             else:
                 print('ERROR, cuadruplo no acceptado: ')
                 print(cuadruplo)
@@ -213,9 +228,9 @@ class maquinaVirtual():
         self.memVirtual.memConst = memoria.memConst
         self.memVirtual.memGlobal = memoria.memGlobal
 
-maquina = maquinaVirtual(dir,cuads)
-maquina.run(0,'END')
-#maquina.memVirtual.printMemoria()
+#maquina = maquinaVirtual(dir,cuads)
+#maquina.run(0,'END')
+
 
 
 
